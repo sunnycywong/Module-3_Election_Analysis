fef38414772b9ecb227b6ecdb9b8d376bcf9019d
swong328 <65827019+swong328@users.noreply.github.com>
2022-02-25 23:56:35 -0500

    Adding PyPoll.py file.

---
+++ PyPoll.py
@@ -0,0 +1,16 @@
+# The data we need to retrieve.
+# 1. The total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+# Use the open statement to open the file as a text file.
+outfile = open(file_to_save, "w")
+# Write some data to the file.
+outfile.write("Hello World")
+
+# Close the file
+outfile.close()
